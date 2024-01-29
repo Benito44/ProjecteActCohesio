b3c2b8fd7b3a1428a13677e803acd3db8677ce46
BenitoMF <b.martinez2@sapalomera.cat>
2024-01-29 15:44:46 +0100

    Iniciar Sessió amb Google dirigit a la pàgina principal

--- Controlador/configuracion.php
+++ Controlador/configuracion.php
@@ -3,7 +3,7 @@
   require '../vendor/autoload.php';
   $clientID = '276558750720-qtno4qc3sv4i22m0pp432no9u9bc58n5.apps.googleusercontent.com';
   $clientSecret = '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';
-  $redirectUri = 'http://localhost/Backend/cohesio/ProjecteActCohesio/Controlador/controlargoogle.php';
+  $redirectUri = 'http://localhost/Backend/cohesio/ProjecteActCohesio/Controlador/llista.php';
 
   // create Client Request to access Google API
   $client = new Google\Client();
