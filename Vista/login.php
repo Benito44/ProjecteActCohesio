35a4698a42d258b7946f37530d782f0e2311e184
BenitoMF <b.martinez2@sapalomera.cat>
2024-02-18 22:11:11 +0100

    vista login

--- Vista/login.php
+++ Vista/login.php
@@ -1,23 +1,21 @@
 <!DOCTYPE html>
-<html lang="ca">
+<html lang="en">
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Iniciar Sessió</title>
+    <title>Iniciar Sesión</title>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
-    <link rel="stylesheet" href="../Estils/estils.css">
 </head>
 <body>
-<div class="container">
+
+<div class="container mt-5">
     <div class="row justify-content-center">
-        <div class="col-lg-6 col-md-8 col-sm-10">
-            <div class="mt-5">
-                <h1 class="text-center">Iniciar Sessió</h1>
-                <p class="lead text-center">Benvingut, si us plau inicia sessió per continuar.</p>
-                <div class="text-center mt-4">
-                    <?php require('../Controlador/autentificacion.php') ?>
-                    <a href="<?php echo $client->createAuthUrl() ?>" class="btn btn-danger btn-lg">Iniciar sessió amb Google</a>
-                </div>
+        <div class="col-lg-4">
+            <h1 class="text-center mb-4">Iniciar Sesión</h1>
+            <div class="text-center">
+                <!-- Enlace de inicio de sesión con Google -->
+                <?php require('../Controlador/autentificacion.php'); ?>
+                <a href="<?php echo $client->createAuthUrl(); ?>" class="btn btn-primary">Iniciar sesión con Google</a>
             </div>
         </div>
     </div>
