2ceaeaf7a0416080586797f426fb3fc0b87a3ee7
BenitoMF <b.martinez2@sapalomera.cat>
2024-02-19 15:27:07 +0100

    canvios controlador

--- Controlador/controlargoogle.php
+++ Controlador/controlargoogle.php
@@ -2,34 +2,10 @@
 
 session_start();
 
-  require 'autentificacion.php';
-  //require '../Model/mainfunction.php';  
+  require 'autentificacion.php'; 
   
   $usuari =  $google_account_info->name;
   $email = $google_account_info->email = $email;
-  echo $usuari;
-  echo $email;
-/*
-  try {
-    if ( comprovarEmail($email)){
-       echo "L'email ja està registrat";
-       $error =  "L'email ja està registrat";
-       //$usuari = encontrarPorEmail($email);
-       $_SESSION['usuari'] = $usuari;
-       header('Location: index.logat.php');
-
-     }else{
-       //insertar_usuari_Oauth2($usuari, $email);
-       $_SESSION['usuari'] = $usuari;
-       header('Location: index.logat.php');
-     }
-     }catch(Exception $e) {
-       echo "Error: " . $e->getMessage();
-     }
-*/
-
-     //TODO: Todos tenemos que tener la misma ruta de carpetas !!!!!
- //  C:\xampp\htdocs\Backend\cohesio\ProjecteActCohesio\Controlador\controlargoogle.php
 
 
 
