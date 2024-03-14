12e7f65f2f13fc53c26ce357adc0b3c27375a80e
AymanSbayy <a.sbay@sapalomera.cat>
2024-03-14 17:59:04 +0100

    enviar i rebre info de les rondes (Conexio front-back)

--- Controlador/definirEvent.php
+++ Controlador/definirEvent.php
@@ -1,88 +1,68 @@
 <?php
 require_once '../Model/consultasbd.php';
-if(isset($_POST['estat'])) {
+if (isset($_POST['estat'])) {
     $estat = $_POST['estat'];
     echo $estat;
-    if ($estat == "R1")
-    {
+    if ($estat == "R1") {
         editarAccio($estat);
-    }
-    else if ($estat == "R2")
-    {
+    } else if ($estat == "R2") {
         editarAccio($estat);
-    }
-    else if ($estat == "R3")
-    {
+    } else if ($estat == "R3") {
         editarAccio($estat);
-    }
-    else if ($estat == "R4")
-    {
+    } else if ($estat == "R4") {
         editarAccio($estat);
-    }
-    else if ($estat == "R5")
-    {
+    } else if ($estat == "R5") {
         editarAccio($estat);
-    }
-    else if ($estat == "R6")
-    {
+    } else if ($estat == "R6") {
         editarAccio($estat);
-    }
-    else if ($estat == "R7")
-    {
+    } else if ($estat == "R7") {
         editarAccio($estat);
-    }
-    else if ($estat == "R8")
-    {
+    } else if ($estat == "R8") {
         editarAccio($estat);
-    }
-    else if ($estat == "R9")
-    {
+    } else if ($estat == "R9") {
         editarAccio($estat);
-    }
-    else if ($estat == "R10")
-    {
+    } else if ($estat == "R10") {
         editarAccio($estat);
-    }
-    else if ($estat == "R11")
-    {
+    } else if ($estat == "R11") {
         editarAccio($estat);
-    }
-    else if ($estat == "R12")
-    {
+    } else if ($estat == "R12") {
         editarAccio($estat);
-    }
-    else if ($estat == "R13")
-    {
+    } else if ($estat == "R13") {
         editarAccio($estat);
-    }
-    else if ($estat == "R14")
-    {
+    } else if ($estat == "R14") {
         editarAccio($estat);
-    }
-    else if ($estat == "R15")
-    {
+    } else if ($estat == "R15") {
         editarAccio($estat);
-    }
-    else if ($estat == "R16")
-    {
+    } else if ($estat == "R16") {
         editarAccio($estat);
-    }
-    else if ($estat == "R17")
-    {
+    } else if ($estat == "R17") {
         editarAccio($estat);
-    }
-    else if ($estat == "R18")
-    {
+    } else if ($estat == "R18") {
         editarAccio($estat);
-    }
-    else if ($estat == "R19")
-    {
+    } else if ($estat == "R19") {
         editarAccio($estat);
-    }
-    else if ($estat == "R20")
-    {
+    } else if ($estat == "R20") {
         editarAccio($estat);
+    } else {
+        editarAccio("Pausat");
     }
-    else {editarAccio("Pausat");}
-} 
+}
 
+
+// Send information to another script
+$data = array('estat' => $estat);
+$url = 'http://example.com/other_script.php';
+$options = array(
+    'http' => array(
+        'method'  => 'POST',
+        'header'  => 'Content-type: application/x-www-form-urlencoded',
+        'content' => http_build_query($data)
+    )
+);
+$context  = stream_context_create($options);
+$result = file_get_contents($url, false, $context);
+if ($result === false) {
+    // Handle error
+} else {
+    // Handle success
+}
