26bd2db2a3d03b080f06326338888f3bc076b269
Marc Peral <m.peral@sapalomera.cat>
2024-03-17 09:59:07 +0100

    afegit petit comentari

--- Middleware/IsAdmin.php
+++ Middleware/IsAdmin.php
@@ -3,6 +3,8 @@
 require_once "LoggedIn.php";
 
 // Comprovem si l'usuari es administrador
+// en cas contrari, el redirigim a la pagina de llista
+// i acabem l'execucio del script
 if ($_SESSION['rol'] != 'admin') {
     header('Location: ../Controlador/llista.php');
     die();
