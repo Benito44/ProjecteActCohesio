12e72403a882cb179436efa6915f8b7c68c5de39
Marc Peral <m.peral@sapalomera.cat>
2024-03-17 11:34:55 +0100

    generat fitxer unic de "header" en la seccio d'admin per centralitzar aquest

--- Vista/ModificarGrupAlumne.vista.php
+++ Vista/ModificarGrupAlumne.vista.php
@@ -12,42 +12,7 @@
 </head>
 
 <body>
-  <header>
-    <div class="container">
-      <nav class="navbar navbar-expand-lg navbar-dark">
-        <div class="container-fluid">
-          <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
-            <span class="navbar-toggler-icon"></span>
-          </button>
-          <div class="collapse navbar-collapse" id="navbarNav">
-            <ul class="navbar-nav ms-auto">
-              <li class="nav-item">
-                <a class="nav-link" href="sessio.php">Llista de grups</a>
-              </li>
-              <li class="nav-item">
-                <a class="nav-link" href="llistaAlumn.php">Llista d'alumness</a>
-              </li>
-              <li class="nav-item">
-                <a class="nav-link" href="afegir.php">Afegir alumnes</a>
-              </li>
-              <li class="nav-item">
-                <a class="nav-link" href="modificarGrupAlumne.php">Modificar grups</a>
-              </li>
-              <li class="nav-item">
-                <a class="nav-link" href="../Vista/Iniciar.php">Iniciar Joc</a>
-              </li>
-              <li class="nav-item">
-                 <a class="nav-link" href="../Controlador/gruposCohesio.php">Enfrontaments</a>
-              </li>
-              <li class="nav-item">
-                <a class="nav-link sessio" href="../Controlador/cerrar_session.php">Tancar Sessió</a>
-              </li>
-            </ul>
-          </div>
-        </div>
-      </nav>
-    </div>
-  </header>
+  <?php include 'AdminHeader.vista.php'; ?>
   <h1>Modificar grup d'alumne</h1>
   <div class="container">
     <div class="row">
